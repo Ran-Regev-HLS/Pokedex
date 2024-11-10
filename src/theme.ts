a5d7c6cb15a0c3783933fbf753f0bbdfce60f154
Ran Regev <ranr@moveohls.com>
2024-11-10 13:24:06 +0200

    added colors and typography

---
+++ src/theme.ts
@@ -0,0 +1,178 @@
+export const colors = {
+  neturalsWhite: "#FFFFFF",
+  neturals100: "#F2F5F7",
+  neturals200: "#A8AEB5",
+  neturals300: "#5A6066",
+  neturals400: "#44484C",
+  neturals500: "#2D3033",
+  primary50: "#EBEFF6",
+  primary100: "#B1BDDB",
+  primary200: "#627BB8",
+  primary300: "#3B5AA6",
+  primary400: "#293F74",
+  primary500: "#182442",
+  secondary100: "#FFFAE6",
+  secondary200: "#FFE069",
+  secondary300: "#FFCB05",
+  secondary400: "#CCA204",
+  secondary500: "#4C3D01",
+};
+
+export const typography = {
+  heading: {
+    xl: {
+      bold: `
+        font-family: 'Mulish', sans-serif;
+        font-size: 28px;
+        font-weight: 700;
+        line-height: 37px;
+      `,
+      medium: `
+        font-family: 'Mulish', sans-serif;
+        font-size: 28px;
+        font-weight: 500;
+        line-height: 37px;
+      `,
+    },
+    lg: {
+      bold: `
+        font-family: 'Mulish', sans-serif;
+        font-size: 24px;
+        font-weight: 700;
+        line-height: 34px;
+      `,
+      medium: `
+        font-family: 'Mulish', sans-serif;
+        font-size: 24px;
+        font-weight: 500;
+        line-height: 34px;
+      `,
+      regular: `
+      font-family: 'Mulish', sans-serif;
+      font-size: 24px;
+      font-weight: 400;
+      line-height: 34px;
+    `,
+    },
+    md: {
+      bold: `
+        font-family: 'Mulish', sans-serif;
+        font-size: 18px;
+        font-weight: 700;
+        line-height: 27px;
+      `,
+      medium: `
+        font-family: 'Mulish', sans-serif;
+        font-size: 18px;
+        font-weight: 500;
+        line-height: 27px;
+      `,
+      regular: `
+        font-family: 'Mulish', sans-serif;
+        font-size: 18px;
+        font-weight: 400;
+        line-height: 27px;
+      `,
+    },
+  },
+  subHeading: {
+    bold: `
+        font-family: 'Mulish', sans-serif;
+        font-size: 16px;
+        font-weight: 700;
+        line-height: 24px;
+      `,
+      medium: `
+        font-family: 'Mulish', sans-serif;
+        font-size: 16px;
+        font-weight: 500;
+        line-height: 24px;
+      `,
+      regular: `
+        font-family: 'Mulish', sans-serif;
+        font-size: 16px;
+        font-weight: 400;
+        line-height: 24px;
+      `,
+  },
+  body: {
+    bold: `
+    font-family: 'Mulish', sans-serif;
+    font-size: 14px;
+    font-weight: 700;
+    line-height: 22px;
+  `,
+  medium: `
+    font-family: 'Mulish', sans-serif;
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 22px;
+  `,
+  regular: `
+    font-family: 'Mulish', sans-serif;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 22px;
+  `,
+  },
+  caption: {
+    bold: `
+    font-family: 'Mulish', sans-serif;
+    font-size: 12px;
+    font-weight: 700;
+    line-height: 19px;
+  `,
+  medium: `
+    font-family: 'Mulish', sans-serif;
+    font-size: 12px;
+    font-weight: 500;
+    line-height: 19px;
+  `,
+  regular: `
+    font-family: 'Mulish', sans-serif;
+    font-size: 12px;
+    font-weight: 400;
+    line-height: 19px;
+  `,
+  },
+  xsmall: {
+    bold: `
+    font-family: 'Mulish', sans-serif;
+    font-size: 11px;
+    font-weight: 700;
+    line-height: 16px;
+  `,
+  medium: `
+    font-family: 'Mulish', sans-serif;
+    font-size: 11px;
+    font-weight: 500;
+    line-height: 16px;
+  `,
+  regular: `
+    font-family: 'Mulish', sans-serif;
+    font-size: 11px;
+    font-weight: 400;
+    line-height: 16px;
+  `,
+  },
+  button: {
+    big: `
+    font-family: 'Mulish', sans-serif;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 20px;
+  `,
+  medium: `
+    font-family: 'Mulish', sans-serif;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 20px;
+  `,
+  small: `
+    font-family: 'Mulish', sans-serif;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 14px;
+  `,
+  },
+};
