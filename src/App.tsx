f7224d3ccc867aabaaba90a2543d279f357aa899
Ran-Regev-HLS <ranr@moveohls.com>
2024-11-13 14:13:02 +0200

    added global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,12 @@
-import { AccountCircle } from "@mui/icons-material";
-import Dropdown from "./assets/components/shared/Dropdown/Dropdown";
-import { Box, Typography } from "@mui/material";
+import GlobalStyle from "./assets/GlobalStyles";
+
 
 function App() {
-  type CustomItem = {
-    id: number;
-    name: string;
-    icon?: React.ReactNode;
-  };
 
-  const stringItems = ["Option 1", "Option 2", "Option 3"];
-  
-  const customItems: CustomItem[] = [
-    { id: 1, name: "Custom Item 1", icon: <AccountCircle /> },
-    { id: 2, name: "Custom Item 2", icon: <AccountCircle /> },
-    { id: 3, name: "Custom Item 3", icon: <AccountCircle /> },
-  ];
-
-  const handleStringChange = (value: string ) => {
-    console.log("Selected string:", value);
-  };
-
-  const handleCustomChange = (value: CustomItem ) => {
-    console.log("Selected custom item:", value);
-  };
 
   return (
     <>
-     <Dropdown<string>
-      
-        items={stringItems}
-        onChange={handleStringChange}
-        />
-
-        <Dropdown<CustomItem>
-        items={customItems}
-        onChange={handleCustomChange}
-        getItemLabel={(item) => String(item.id)}
-        renderItemContent={(item) => (
-          <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
-            {item.icon}
-            <Typography>{item.name}</Typography>
-          </Box>
-        )}
-      />
+    <GlobalStyle />
       </>
   );
 }
